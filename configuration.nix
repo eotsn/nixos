ff5e2b20c9022d386f6e2c6bf0450758dc2688f5
Eric Ottosson <noreply@eotsn.com>
2025-06-13 19:35:53 +0200

    Replace COSMIC desktop with GNOME

--- configuration.nix
+++ configuration.nix
@@ -54,9 +54,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the COSMIC Desktop Environment.
-  services.displayManager.cosmic-greeter.enable = true;
-  services.desktopManager.cosmic.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
