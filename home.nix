87ee58aea62d53889338d56e0a76f6eafa19f235
Eric Ottosson <noreply@eotsn.com>
2025-06-22 00:07:48 +0200

    Update fzf color theme

--- home.nix
+++ home.nix
@@ -61,6 +61,23 @@ in
 
     programs.fzf = {
       enable = true;
+      colors = {
+        "bg" = "#1E1E2E";
+        "bg+" = "#313244";
+        "border" = "#313244";
+        "fg" = "#CDD6F4";
+        "fg+" = "#CDD6F4";
+        "header" = "#F38BA8";
+        "hl" = "#F38BA8";
+        "hl+" = "#F38BA8";
+        "info" = "#CBA6F7";
+        "label" = "#CDD6F4";
+        "marker" = "#B4BEFE";
+        "pointer" = "#F5E0DC";
+        "prompt" = "#CBA6F7";
+        "selected-bg" = "#45475A";
+        "spinner" = "#F5E0DC";
+      };
     };
 
     programs.jq = {
